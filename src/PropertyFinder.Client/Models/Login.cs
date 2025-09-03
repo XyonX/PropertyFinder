a0046181ee85eb0c0fd721718599127c1007804c
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2025-09-03 23:59:40 +0530

    Add Login and Register models and solution file
    Introduced Login and Register model classes for user authentication and registration data. Added the initial Visual Studio solution file for PropertyFinder.Client.

---
+++ src/PropertyFinder.Client/Models/Login.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace PropertyFinder.Client.Models
+{
+    public class Login
+    {
+        public String Email { get; set; }
+        public String Password { get; set; }
+    }
+}
---
+++ src/PropertyFinder.Client/Models/Register.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace PropertyFinder.Client.Models
+{
+    public class Register
+    {
+        public String Name { get; set; }
+        public String Email { get; set; }
+
+        public String Password { get; set; }
+
+        public String Phone { get; set; }
+    }
+}
